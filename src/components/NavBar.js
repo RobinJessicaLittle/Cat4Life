183eeb5e7c0c95e1df61bcdff9d45f61d41a9d05
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-04-21 16:34:51 +0100

    navbar, images

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import { HeaderContainer, HeaderWrapper, Logo, NavbarRight, Crate, Crateimage, WelcomePage, WelcomeText, WelcomeImage, Sectionbreak} from './NavBar.styled';
+import logo from '../logo.jpeg';
+import welcome from '../welcome.jpeg';
+import emptyCrate from '../emptyCrate.jpeg';
+
+const Navbar = () => {
+	return (
+
+		<HeaderContainer>
+            <HeaderWrapper>
+                <Logo src={logo}>
+
+                </Logo>
+                <NavbarRight>
+                    <Crate> 
+                        <Crateimage src={emptyCrate} />
+                    </Crate>
+                </NavbarRight>
+            </HeaderWrapper>
+
+            <WelcomePage>
+                <WelcomeText> Every year thousands of Cats are offered up for adoption. For many, YOU may be the only chance they have at better life in a lovign home. </WelcomeText>
+                <WelcomeImage src= {welcome} alt="group of kittens" />
+            </WelcomePage>
+            <Sectionbreak> View our current Orphans </Sectionbreak>
+        </HeaderContainer>
+
+        
+	);
+};
+
+export default Navbar
